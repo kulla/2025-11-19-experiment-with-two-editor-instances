9465d00d5e7c574dc79a62f9b6a8431b4fef7ee6
Stephan Kulla <git.mail@kulla.me>
2025-11-19 23:42:12 +0100

    feat: Add prosekit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import '@picocss/pico/css/pico.min.css'
 import './App.css'
 
+import 'prosekit/basic/style.css'
+import 'prosekit/basic/typography.css'
+
+import { defineBasicExtension } from 'prosekit/basic'
+import { createEditor } from 'prosekit/core'
+import { ProseKit } from 'prosekit/react'
+import { useMemo } from 'react'
+
 export default function App() {
+  const editor = useMemo(() => {
+    const extension = defineBasicExtension()
+    return createEditor({ extension })
+  }, [])
+
   return (
     <main className="p-10">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+      <ProseKit editor={editor}>
+        <div ref={editor.mount} className="border-2 rounded-xl p-4"></div>
+      </ProseKit>
     </main>
   )
 }
